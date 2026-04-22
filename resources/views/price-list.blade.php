e5eb9fac27f89b9dc44075f4da2c9412f7b9ba80
digital-nellemann <digital@nellemann.dk>
2026-04-22 08:34:14 +0200

    Update price-list.blade.php

--- resources/views/price-list.blade.php
+++ resources/views/price-list.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ 'Prisliste - ' . $car->name }}</title>
+
         @vite(['resources/css/price-list.css'])
     </head>
     <body>
@@ -285,6 +288,7 @@
                             <div class="w-24">
                                 <p class="text-center">
                                     {{ $trim->name }}
+
                                     @if ($trim->uses_high_tax)
                                         @php
                                             $usesHighTax = true
@@ -292,6 +296,10 @@
 
                                         **
                                     @else
+                                        @php
+                                            $usesHighTax = false
+                                        @endphp
+
                                         *
                                     @endif
                                 </p>
@@ -406,6 +414,10 @@
 
                                         **
                                     @else
+                                        @php
+                                            $usesHighTax = false
+                                        @endphp
+
                                         *
                                     @endif
                                 </p>
