03da01520d0288276d40e24d5c40e63fa3d95145
digital-nellemann <digital@nellemann.dk>
2026-04-28 13:54:18 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return Inertia::render('auth/login');
 })->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth'])->group(function () {
 
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
