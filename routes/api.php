4d7d6a8f44075c2f7169a34a2341afab767e04ce
digital-nellemann <digital@nellemann.dk>
2026-09-09 12:40:09 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -12,9 +12,6 @@ use App\Http\Controllers\Api\SparePartController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-    Route::get('/spare-parts/{sparePart}', [SparePartController::class, 'show']);
-
-
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', fn (Request $request) => $request->user());
 
@@ -62,7 +59,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/stock-cars', [StockCarController::class, 'index']);
     Route::get('/stock-cars-price-range', [StockCarController::class, 'priceRange']);
 
-    //Route::get('/spare-parts/{sparePart}', [SparePartController::class, 'show']);
+    Route::get('/spare-parts/{sparePart}', [SparePartController::class, 'show']);
 
     Route::get('/compliance-text', [ComplianceTextController::class, 'show'])->name('compliance.text');
 
