6138f078c7d00552cc6333f962b32fb72bc1bdc5
digital-nellemann <digital@nellemann.dk>
2026-09-10 09:16:39 +0200

    Update SparePartController.php

--- app/Http/Controllers/Api/SparePartController.php
+++ app/Http/Controllers/Api/SparePartController.php
@@ -13,7 +13,16 @@ class SparePartController extends Controller
         PimService $pimService,
         string $partNumber
     ) {
-        $id = $productsSearchRequest->getSparePartId($partNumber);
+        $id = $productsSearchRequest->getSparePartId(
+            '2b23c5e6-b02a-43ff-8178-0837725f92b8',
+            $partNumber
+        );
+
+        if (!$id) {
+            return response()->json([
+                'message' => 'Spare part not found.',
+            ], 404);
+        }
 
         $sparePartData = $pimService->getSpacePart($id);
 
